3143ba2d1d69b7197fcf853a6ec23c0e1d87e37d
Etienne CASSIN <ecassin@campus-eni.fr>
2024-07-31 14:24:15 +0200

    utilisation des services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import {FormsModule} from "@angular/forms";
 import {NgClass, NgIf} from "@angular/common";
 import {UnPtitNomComponent} from "./un-ptit-nom/un-ptit-nom.component";
 import {ContactListComponent} from "./contact-list/contact-list.component";
+import {SaisieUtilisateurComponent} from "./saisie-utilisateur/saisie-utilisateur.component";
+import {ListeUtilisateursComponent} from "./liste-utilisateurs/liste-utilisateurs.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgClass, UnPtitNomComponent, ContactListComponent, NgIf],
+  imports: [RouterOutlet, FormsModule, NgClass, UnPtitNomComponent, ContactListComponent, NgIf, SaisieUtilisateurComponent, ListeUtilisateursComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
